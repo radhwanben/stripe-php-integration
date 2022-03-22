07be1065cc1221975d0b14960e98274e7c27ae02
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-03-22 21:38:16 +0100

    latest

--- config/webhook.php
+++ config/webhook.php
@@ -162,7 +162,7 @@ switch ($event->type) {
     // sending email for confirmation containing tickets codes
     $recieverEmail = $data->email;
     $subject = "The Dubai Life";
-    $email_template ="views/mail.phtml";
+    $email_template ="views/mail.html";
     $body = file_get_contents($email_template);
     $mailer = new Mail($SMTP_USER,$SMTP_PASSWORD,$SMTP_HOST,$SMTP_PORT);
     $mailer->sendMail($recieverEmail,$subject,$body);
