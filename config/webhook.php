ebc58b8ce51ebe48949138302076c74de7e7a8f9
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-03-22 22:55:56 +0100

    latest

--- config/webhook.php
+++ config/webhook.php
@@ -167,6 +167,18 @@ switch ($event->type) {
     $body =str_replace('%email%', $data->email, $body);
     $body =str_replace('%phone%', $data->phone, $body);
     $body =str_replace('%link%', $paymentIntent->charges->data[0]->receipt_url, $body);
+    foreach ($ticketlist as $ticket){
+      $body =str_replace('%tickets%' , '
+      
+      <div class="notifLine">
+            <img src="http://dubailife3.herokuapp.com/assets/images/ticket.png" alt="ticket" />
+            <p>Ticket N-<span>1</span> :</p>
+            <p class="ticketnumber">'.  $ticket . '</p>
+          </div>
+      
+      
+      ',$body);
+    }
     $body =str_replace('%download%' ,'  <div class="box5">
               <p>Download your ebook from here:</p>
                 <button>Download</button>
