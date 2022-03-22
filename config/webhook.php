c0366b0807e813b2708bc4892dd306eb565083ba
ayedi <ayedi@Amines-MacBook-Pro.local>
2022-03-22 21:03:53 +0100

    latest

--- config/webhook.php
+++ config/webhook.php
@@ -163,7 +163,7 @@ switch ($event->type) {
     $recieverEmail = $data->email;
     $subject = "The Dubai Life";
     $email_template ="../views/mail.php";
-    $body = file_get_contents($email_template);;
+    $body = file_get_contents($email_template);
     $mailer = new Mail($SMTP_USER,$SMTP_PASSWORD,$SMTP_HOST,$SMTP_PORT);
     $mailer->sendMail($recieverEmail,$subject,$body);
 
